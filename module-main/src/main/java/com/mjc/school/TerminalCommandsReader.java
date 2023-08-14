f1f321265dd16d3e019d9acaed881aeb758e953b
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-08-14 15:48:37 +0500

    some codes added module mian

---
+++ module-main/src/main/java/com/mjc/school/TerminalCommandsReader.java
@@ -0,0 +1,53 @@
+package com.mjc.school;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class TerminalCommandsReader {
+
+    private static final String PROMPT_ENTER_NUMBER_OF_OPERATION
+            = "_________\n" +
+            "Enter the number of operation:";
+
+    private final Scanner sc = new Scanner(System.in);
+
+    public Optional<Commands> getCommand() {
+        printCommandPrompt();
+        if (sc.hasNextLine()) {
+            try {
+                int commandCode = Integer.parseInt(sc.nextLine());
+                Commands command = getCommandByCode(commandCode);
+                return Optional.ofNullable(command);
+            } catch (NumberFormatException e) {
+                return Optional.empty();
+            }
+        }
+        return Optional.empty();
+    }
+
+    public String requestResponseByPrompt(String prompt) {
+        System.out.println(prompt);
+        return sc.nextLine();
+    }
+
+    private Commands getCommandByCode(int commandCode) {
+        for (Commands cmd : Commands.values()) {
+            if (cmd.i == commandCode)
+                return cmd;
+        }
+        return null;
+    }
+
+    private void printCommandPrompt() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PROMPT_ENTER_NUMBER_OF_OPERATION).append('\n');
+        for (Commands cmd : Commands.values()) {
+            sb
+                    .append(cmd.i)
+                    .append(" - ")
+                    .append(cmd.desc)
+                    .append('\n');
+        }
+        System.out.print(sb);
+    }
+}
