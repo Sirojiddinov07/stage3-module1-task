fba853d4b0949f6bd732489ca80b069adceb4e21
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-08-15 15:49:03 +0500

    some codes added

--- module-main/src/main/java/com/mjc/school/MainApp.java
+++ module-main/src/main/java/com/mjc/school/MainApp.java
@@ -1,5 +1,11 @@
 package com.mjc.school;
 
+import com.mjc.school.exeptions.AuthorNotFoundException;
+import com.mjc.school.exeptions.IdMustBeNmberExeption;
+import com.mjc.school.exeptions.NewsNotFoundException;
+import com.mjc.school.servise.exeptions.NewsContentInvalidException;
+import com.mjc.school.servise.exeptions.NewsTitleInvalidException;
+
 public class MainApp {
 
     private static final String Command_Not_Found ="Command Not Found";
@@ -8,14 +14,20 @@ public class MainApp {
 
     public static void main(String[] args) {
         TerminalCommandsReader terminalCommandsReader = new TerminalCommandsReader();
-        CommandsExecutor commandsExecutor = new CommandsExecutor();
-
+        CommandsExec commandsExecutor = new CommandsExec();
         while (true){
             terminalCommandsReader.getCommand().ifPresent(cmd ->{
                 try {
-                    commandsExec.g
+                    commandsExecutor.executeCommand(cmd);
+                }catch (NewsNotFoundException |
+                        AuthorNotFoundException |
+                        NewsTitleInvalidException |
+                        NewsContentInvalidException |
+                        IdMustBeNmberExeption e) {
+                    System.out.println(e.getMessage());
                 }
-            });
+                    }
+            );
         }
     }
 }
